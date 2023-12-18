f181c74472955c929895cb0f5760b770efc74ba0
gaia <gaialibera2001@gmail.com>
2023-12-18 21:28:22 +0100

    manca solo animazione js

--- animation.js
+++ animation.js
@@ -1,1 +1,13 @@
+const sidebar = document.querySelector('.menu');
+const sidebarToggler = document.querySelector('.hamburger');
 
+sidebarToggler.addEventListener('click', () => {
+    sidebar.classList.toggle('showmenu');
+});
+
+
+window.addEventListener('click', (e) => {
+    if (e.target.id !== 'menu' && e.target.className !== 'hamburger') {
+        sidebar.classList.remove('showmenu');
+    }
+});
