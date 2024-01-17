9c718a93d54ebb9f2b975c5aa01aab5bac556280
gaia <gaialibera2001@gmail.com>
2024-01-17 14:22:41 +0100

    Slideshow in Javascript + responsive

--- animation.js
+++ animation.js
@@ -1,3 +1,5 @@
+
+// MENU ANIMATION
 const menu = document.querySelector('#menu')
 const toggle = document.querySelector('#toggle')
 
@@ -9,3 +11,25 @@ const toggleMenu = () => {
 document.addEventListener('DOMContentLoaded', () =>{
   toggle.addEventListener('click', toggleMenu)
 })
+
+//SLIDESHOW
+document.addEventListener("DOMContentLoaded", function() {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.slide');
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.classList.add('active');
+      } else {
+        slide.classList.remove('active');
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+  setInterval(nextSlide, 4000);
+});
