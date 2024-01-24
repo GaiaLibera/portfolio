08040576afa33615fd9ca98242e16e1e646a3149
gaia <gaialibera2001@gmail.com>
2024-01-24 14:37:44 +0100

    non ricordo

--- animation.js
+++ animation.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let currentSlide = 0;
   const slides = document.querySelectorAll('.slide');
 
-  function showSlide(index) {
+  function slideshow(index) {
     slides.forEach((slide, i) => {
       if (i === index) {
         slide.classList.add('active');
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function nextSlide() {
     currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
+    slideshow(currentSlide);
   }
   setInterval(nextSlide, 4000);
 });
